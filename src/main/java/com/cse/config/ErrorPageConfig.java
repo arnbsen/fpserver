92b0a90414bcf95e5a636f344cb64f008c38ac15
Arnab Sen <arnb.sen@hotmail.com>
2019-06-05 11:51:18 +0530

    Resolving Imports

--- src/main/java/com/cse/config/ErrorPageConfig.java
+++ src/main/java/com/cse/config/ErrorPageConfig.java
@@ -1,6 +1,14 @@
 package com.cse.config;
-@Configuration    
+
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.ErrorPageRegistrar;
+import org.springframework.boot.web.server.ErrorPageRegistry;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+
+@Configuration
 public class ErrorPageConfig implements ErrorPageRegistrar {
+
 
     @Override
     public void registerErrorPages(ErrorPageRegistry registry) {
