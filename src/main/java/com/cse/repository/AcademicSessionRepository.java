6caf99aca687dc8ce9471f04bf50b44f943a2bd8
Arnab Sen <arnb.sen@hotmail.com>
2019-08-31 18:02:45 +0530

    Redeploy

--- src/main/java/com/cse/repository/AcademicSessionRepository.java
+++ src/main/java/com/cse/repository/AcademicSessionRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 
+
 /**
  * Spring Data MongoDB repository for the AcademicSession entity.
  */
